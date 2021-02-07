0cc209e58449a9e91c3f8bcf0dac6b6ff3ac73bd
soulmate026 <75714617+soulmate026@users.noreply.github.com>
2021-02-07 20:45:52 +0900

    serah - 회원가입 및 id/pwd찾기 일부 db연결

---
+++ mini_hobby/src/MemberDAO.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO extends Main3DBConnection{
+
+	public MemberDAO() {
+		
+	}
+	// 레코드 추가 - 회원등록
+	public int memberInsert(MemberVO vo) {
+		int result=0;
+		try {
+			getConn();
+			
+			sql = "insert into membertbl(id, pwd, name, birth, mail, tel, addr, sort) "
+					+ " values(?,?,?,?,?,?,?,?)";
+			// login_date는 sysdate가 default라서 뺐음
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getId());
+			pstmt.setString(2, vo.getPwd());
+			pstmt.setString(3, vo.getName());
+			pstmt.setString(4, vo.getBirth());
+			pstmt.setString(5, vo.getMail());
+			pstmt.setString(6, vo.getTel());
+			pstmt.setString(7, vo.getAddr());
+			pstmt.setInt(8, vo.getSort());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			dbClose();
+		}
+		return result;
+	}
+	// 메일주소로 아이디 찾기
+	public List<MemberVO> idSearch(String searchId) {
+		List<MemberVO> lst = new ArrayList<MemberVO>();
+		try {
+			getConn();
+			
+			sql = "select id, name, mail from membertbl where mail=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, searchId);
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				MemberVO vo = new MemberVO();
+				vo.setId(rs.getString(1));
+				vo.setName(rs.getString(2));
+				vo.setMail(rs.getString(3));
+				
+				lst.add(vo);
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			dbClose();
+		}
+		return lst;
+	}
+	// 아이디와 메일주소로 패스워드 찾기
+	public List<MemberVO> pwdSearch(String searchId, String searchMail) {
+		List<MemberVO> lst = new ArrayList<MemberVO>();
+		try {
+			getConn();
+			
+			sql = "select id, pwd, name, mail from membertbl where id=? and mail=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, searchId);
+			pstmt.setString(2, searchMail);
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				MemberVO vo = new MemberVO();
+				vo.setId(rs.getString(1));
+				vo.setPwd(rs.getString(2));
+				vo.setName(rs.getString(3));
+				vo.setMail(rs.getString(4));
+				
+				lst.add(vo);
+			}
+		
+		} catch(Exception e) {
+			e.printStackTrace();	
+		} finally {
+			dbClose();
+		}
+		return lst;
+	}
+
+}
---
+++ mini_hobby/src/MemberVO.java
@@ -0,0 +1,124 @@
+
+public class MemberVO {
+	String id;
+	String pwd;
+	String name;
+	String birth;
+	String mail;
+	String tel;
+	String addr;
+	String login_date;
+	int sort;
+	
+
+	public MemberVO() {
+		
+	}
+	public MemberVO(String mail) {
+		this.mail = mail;
+	}
+	public MemberVO(String id, String mail) {
+		this.id = id;
+		this.mail = mail;
+	}
+	public MemberVO(String id, String pwd, String name, String birth, String mail, String tel, String addr, int sort) {
+		this(id, mail);
+		this.pwd = pwd;
+		this.name = name;
+		this.birth = birth;
+		this.tel = tel;
+		this.addr = addr;
+		this.sort = sort;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getPwd() {
+		return pwd;
+	}
+
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getBirth() {
+		return birth;
+	}
+
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+
+	public String getMail() {
+		return mail;
+	}
+
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+
+	public String getTel() {
+		return tel;
+	}
+
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+
+	public String getAddr() {
+		return addr;
+	}
+
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+
+	public String getLogin_date() {
+		return login_date;
+	}
+
+
+	public void setLogin_date(String login_date) {
+		this.login_date = login_date;
+	}
+
+
+	public int getSort() {
+		return sort;
+	}
+
+
+	public void setSort(int sort) {
+		this.sort = sort;
+	}
+
+}
