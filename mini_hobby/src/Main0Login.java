55e16f8876c543efc2adbc7d1b72f8cc5a97f76e
HyunJay52 <76993044+HyunJay52@users.noreply.github.com>
2021-02-09 11:33:19 +0900

    02/09 textfield imort

--- mini_hobby/src/Main0Login.java
+++ mini_hobby/src/Main0Login.java
@@ -14,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
 import administrator.Admin0Login;
