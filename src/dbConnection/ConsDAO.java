a2d88e0f61cf2663ea00158434d5753fa33b32e4
HyunJay52 <hyunjungk52@gmail.com>
2021-02-12 17:03:35 +0900

    02/12 선생님 보낸 메시지함, 다이얼로그 뜨는지 확인하기..!, 클래스 패스 뺴고 올림

--- src/dbConnection/ConsDAO.java
+++ src/dbConnection/ConsDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ConsDAO extends DBConnection{
 
 	public ConsDAO() {}
-	//받은메세지
+	//받은메세지, 보낸메세지
 	public List<ConsVO> studenMsgRec(String id){
 		List<ConsVO> lst = new ArrayList<ConsVO>();
 		
@@ -55,7 +55,7 @@ public class ConsDAO extends DBConnection{
 		
 		try {
 			getConn();
-			
+			//select msg_num, get, send, msg_title, msg_detail, send_time from constbl where send='hyunjay52' and msg_num='5';			
 			sql = "select msg_num, get, send, msg_title, msg_detail, to_char(send_time, 'YYYY/MM/DD HH:MI') from constbl where get = ? and msg_num = ?";
 			
 			pstmt = conn.prepareStatement(sql);
@@ -67,6 +67,23 @@ public class ConsDAO extends DBConnection{
 				ConsVO vo = new ConsVO(rs.getInt(1),rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
 				lst.add(vo);
 			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			dbClose();
+		}
+		
+		return lst;
+	}
+	//보낸메세지 다이어로그 
+	public List<ConsVO> userSendDiaRec(String id, int msgNum){
+		List<ConsVO> lst = new ArrayList<ConsVO>();
+		try {
+			getConn();
+			sql= "select msg_num, get, send, msg_title, msg_detail, to_char(send_time, 'YYYY/MM/DD HH:MI' from constbl where send = ? and msg_num = ? ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setInt(2, msgNum);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
